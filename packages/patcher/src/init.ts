45cd40a311b0eaf52b2cf682ec90a2d094117c8e
YUVRAJ <yuvraj141104@gmail.com>
2026-06-30 18:03:05 +0530

    fix(patcher): polyfill __dirname for ESM context to fix visora init crash

--- packages/patcher/src/init.ts
+++ packages/patcher/src/init.ts
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
 import chalk from 'chalk';
 import ora from 'ora';
 import { execSync } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const SUCCESS = chalk.green;
 const FAIL = chalk.red;
